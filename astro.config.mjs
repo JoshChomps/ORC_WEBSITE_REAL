639f5f201215a06e2ab7a164511d69f13b0ea294
Josh <joshchomiak@gmail.com>
2026-04-09 13:59:51 -0700

    Standardize routing, fix Vercel build errors, and overhaul project for root hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://joshchomps.github.io',
-  base: '/ORC_WEBSITE_REAL',
+  base: '/',
   trailingSlash: 'always',
   build: {
     format: 'directory'
